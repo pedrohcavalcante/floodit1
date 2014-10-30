a944d3070c5a1df58de24f4014953c2a08a46be8
pedrohcavalcante <pedrinho.sjs@outlook.com>
2014-10-30 14:37:54 -0300

    Create gera.h

---
+++ gera.h
@@ -0,0 +1,19 @@
+#ifndef __MATRIZ
+#define __MATRIZ
+
+/* --- Tipos Definidos --- */
+
+typedef struct {
+	int tamanho;
+} gera_mat;
+
+/* --- Sub-rotinas --- */
+
+/**
+* Gera a matriz com números aleatórios.
+* @param tamanho é o tamanho do vetor que o usuário insere.
+*/
+
+Gerar (gera_mat tamanho);
+
+#endif
